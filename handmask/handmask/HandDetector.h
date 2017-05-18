da3ec767ce88b2e63dfea33d0efbeec830679849
L.Laddie-work <liuhan132@foxmail.com>
2017-05-18 18:51:56 +0800

    Add convex hull of hand mask

--- handmask/handmask/HandDetector.h
+++ handmask/handmask/HandDetector.h
@@ -7,23 +7,40 @@ using namespace cv;
 using namespace std;
 
 enum ColorSpaceType {
-	LC_YCrCb, LC_LAB, LC_HSV
+	C_YCrCb, C_YUV, C_HSV
 };
 
-#define MIN_Cr 120
-#define MAX_Cr 180
-#define MIN_Cb 80
-#define MAX_Cb 135
+#define MIN_Cr 113
+#define MAX_Cr 173
+#define MIN_Cb 77
+#define MAX_Cb 127
 #define MIN_Y 30
+
+#define MIN_Y2 65
+#define MAX_Y2 170
+#define MIN_U 85
+#define MAX_U 140
+#define MIN_V 85
+#define MAX_V 160
+
+#define THRESHOLD_AREA 30
 
 class HandDetector
 {
 public:
 	HandDetector(int tar_width);
+
+	void train(const Mat& img, const Mat& mask);
+
 	void process(const Mat& src, Mat& des);
 	void process2(const Mat& src, Mat& des);
 
+	void hand_contours(const Mat& src, Mat& dst);
+
+	void threashold_C(const Mat& src, Mat& dst, int color_code);
 	void threashold_YCrCb(const Mat& src, Mat& dst);
+	void threashold_YUV(const Mat& src, Mat& dst);
+
 	void threashold_Y(const Mat& src, Mat& dst);
 
 private:
--- handmask/handmask/main.cpp
+++ handmask/handmask/main.cpp
@@ -13,15 +13,47 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	int train = 0;				// train or test
-	int tar_width = 500;		// resizing the input image
+	int tar_width = 320;		// resizing the input image
 
 	char buffer[_MAX_PATH];
 	getcwd(buffer, _MAX_PATH);
 	string root = buffer;
 
+	HandDetector hd(tar_width);
+
 	if (train)
 	{
 		cout << "Training Model...\n";
+
+		ifstream fs(root + "\\img_name.txt");
+		if (!fs.is_open())
+		{
+			cerr << "Error: cannot open file\n";
+			return -1;
+		}
+
+		Mat img, mask;
+		while (!fs.eof())
+		{
+			string name;
+			fs >> name;
+
+			img = imread(root + "\\img\\" + name);
+			if (!img.data)
+				break;
+
+			mask = imread(root + "\\mask\\" + name);
+			if (!img.data)
+				break;
+
+			//hd.train(img, mask);
+
+			cout << name << endl;
+
+			hd.process(img, mask);
+			imshow("mask", mask);
+			waitKey(0);
+		}
 	}
 	else
 	{
@@ -35,8 +67,10 @@ int main(int argc, char* argv[])
 			return -1;
 		}
 
-		HandDetector hd(tar_width);
 		Mat img, mask;
+		int cnt = 0;
+		time_t start, stop;
+		start = time(NULL);
 		while (1)
 		{
 			cap >> img;
@@ -44,8 +78,19 @@ int main(int argc, char* argv[])
 				break;
 
 			hd.process(img, mask);
+
 			imshow("mask", mask);
 			waitKey(1);
+
+			// calculate and display fps every 2s
+			cnt++;
+			stop = time(NULL);
+			if (stop - start > 2)
+			{
+				cout << "fps: " << cnt / (stop - start) << endl;
+				start = time(NULL);
+				cnt = 0;
+			}
 		}
 	}
 }
